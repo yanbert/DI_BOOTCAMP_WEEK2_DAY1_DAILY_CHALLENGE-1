d56fb1139a2de4153e8d583457a0ce9d5bc2a81f
yanbert <yanbert@gmail.com>
2022-12-30 01:38:07 +0000

    Merci de voir mes commentaires dans le code

--- exercice2.js
+++ exercice2.js
@@ -1,7 +1,11 @@
 const moreFruits = ["Banana", ["Apples", ["Oranges"], "Blueberries"]];
 
 /* acceder au tableau pour obtenir "Oranges" */
+/**************************************************
 
+Mauvaise reponse, merci de revoir cet exercice
+
+*************************************************/
 moreFruits[1][1][0];
 console.log(moreFruits);
 
